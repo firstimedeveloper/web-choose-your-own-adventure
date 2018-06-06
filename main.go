e1b8847b0d34450bbae293280cc29aaa892df01c
firstimedeveloper <hanjun.1998@gmail.com>
2018-06-06 23:26:19 +0900

    modified import statements

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -38,6 +38,7 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+
 	http.Error(w, "Chapter not found", http.StatusNotFound)
 
 }
@@ -57,16 +58,14 @@ func main() {
 	}
 	//fmt.Print("Parsed Json file: ", parsedJSON)
 
-	log.Fatal(http.ListenAndServe(":8080", NewHandler(parsedJSON)))
+	port := os.Getenv("PORT")
 
-	fmt.Println("Welcome to Choose your Own Adventure!\nAn interactive story where you dictate what happens.\nPress enter to continue.")
-	var temp string
-	fmt.Scanln(&temp)
-	storyOver := false
-	for !storyOver {
-		startArc("intro", parsedJSON)
-		storyOver = true
+	if port == ":" {
+		log.Fatal("$PORT must be set")
 	}
+
+	log.Fatal(http.ListenAndServe(":"+port, NewHandler(parsedJSON)))
+
 }
 
 //StoryArc is a struct that contains the title, storylines, and options
@@ -80,35 +79,4 @@ type StoryArc struct {
 type Option struct {
 	Text string
 	Arc  string
-}
-
-func startArc(key string, parsedJSON map[string]StoryArc) {
-	//print the title of the StoryArc
-	fmt.Println(parsedJSON[key].Title)
-	//print story lines secuentially, requiring the user to press enter inbetween each line
-	var temp int
-	for _, lines := range parsedJSON[key].Story {
-		fmt.Println(lines)
-		fmt.Scanln(&temp)
-	}
-
-	if len(parsedJSON[key].Options) == 0 {
-		fmt.Println("The End. What a great Adventure!")
-		return
-	}
-
-	var count int
-	for index, option := range parsedJSON[key].Options {
-		fmt.Printf("Press %d: %s\n", index+1, option.Text)
-		count++
-	}
-	fmt.Print("Enter a number: ")
-	fmt.Scanln(&temp)
-	for temp < 1 || temp > count {
-		fmt.Print("Please enter a valid number: ")
-		fmt.Scanln(&temp)
-	}
-
-	startArc(parsedJSON[key].Options[temp-1].Arc, parsedJSON)
-
 }
